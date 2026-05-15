559da88c8e322baf248ea2fc4ffa8a84585f477c
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-05-15 15:51:05 +0530

    fact and binomail Coeff.

--- functions.java
+++ functions.java
@@ -16,14 +16,39 @@ public class functions {
         return prod;
     }
 
+    public static int factorial(int n){
+        int fact = 1;
+        for(int i = 1;i<=n;i++){
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public static int binCoeff(int n, int r){
+        int n_fact = factorial(n);
+        int r_fact = factorial(r);
+        int nmr_fact = factorial(n-r);
+        int biCoeff = n_fact/(r_fact*nmr_fact);
+        return biCoeff;
+    }
+
     public static void main(String[] args){
          Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int calculateSum = sum(a,b);
-        System.out.println("sum = "+ calculateSum);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
 
-        int calculateProd = multiply(a,b);
-        System.out.println("prod = "+ calculateProd);
-    }    
+        // int calculateSum = sum(a,b);
+        // System.out.println("sum = "+ calculateSum);
+
+        // int calculateProd = multiply(a,b);
+        // System.out.println("prod = "+ calculateProd);
+    
+        int fact = factorial(7);
+        System.out.println(fact);
+        
+        int binoCoeff =  binCoeff(5,4);
+        System.out.println(binoCoeff);
+
+    }   
+
 }
