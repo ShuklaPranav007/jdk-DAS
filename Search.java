95cc94eda4436bc6d2444416a0849a46bd04f32b
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-06-14 15:44:31 +0530

    pairs

--- Search.java
+++ Search.java
@@ -39,9 +39,30 @@ public class Search{
         return -1;
     }
 
+    public static void reverseArray(int num[]){
+        int first = 0;
+        int last = num.length-1;
+        while(first<last){
+            int temp = num[last];
+            num[last]= num[first];
+            num[first] = temp;
+
+            first++;
+            last--;
+        }
+    }
+
+    public static void printPairs(int num[]){
+        for(int i =0;i<num.length;i++){
+            int curr = num[i];
+            for(int j = i+1;j<num.length;j++){
+                System.out.print(("("+curr+","+ num[j]+")"));
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         int num[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-        int key = 8;
-                
+        printPairs(num);
     }
 }
