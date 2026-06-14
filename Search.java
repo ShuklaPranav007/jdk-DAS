59e3d65b1314b2db320cc7720f42da6550c1e278
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-06-14 16:54:57 +0530

    subarray, sum, max, min

--- Search.java
+++ Search.java
@@ -1,50 +1,51 @@
 
-public class Search{
-    public static int linearSearch(int num[], int key){
-        for (int i = 0; i<num.length;i++){
-            if(num[i]==key){
-                return i+1;
+public class Search {
+
+    public static int linearSearch(int num[], int key) {
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] == key) {
+                return i + 1;
             }
         }
         return -1;
     }
 
-    public static int getLargestValue(int num[]){
+    public static int getLargestValue(int num[]) {
         int largest = Integer.MIN_VALUE;
         // int smallest = Integer.MAX_VALUE;
-        for(int i=0;i<num.length;i++){
-            if(num[i]>largest){
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] > largest) {
                 largest = num[i];
             }
         }
         return largest;
     }
 
-    public static int BinarySearch(int num[],int key){
-        int start = 0; 
-        int end = num.length-1;
-        while(start <= end){
-            int mid = (start+end)/2;
+    public static int BinarySearch(int num[], int key) {
+        int start = 0;
+        int end = num.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
 
             // compariosn
-            if(num[mid]==key){
-                return mid+1;
+            if (num[mid] == key) {
+                return mid + 1;
             }
-            if(num[mid]<key){
-                start = mid+1;
-            }else{
-                end = mid-1;
+            if (num[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
         return -1;
     }
 
-    public static void reverseArray(int num[]){
+    public static void reverseArray(int num[]) {
         int first = 0;
-        int last = num.length-1;
-        while(first<last){
+        int last = num.length - 1;
+        while (first < last) {
             int temp = num[last];
-            num[last]= num[first];
+            num[last] = num[first];
             num[first] = temp;
 
             first++;
@@ -52,17 +53,43 @@ public class Search{
         }
     }
 
-    public static void printPairs(int num[]){
-        for(int i =0;i<num.length;i++){
-            int curr = num[i];
-            for(int j = i+1;j<num.length;j++){
-                System.out.print(("("+curr+","+ num[j]+")"));
+    public static void subArray(int num[]) {
+        int ts = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int minSum = Integer.MAX_VALUE;
+        for (int i = 0; i < num.length; i++) {
+            for (int j = i; j < num.length; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    System.out.print(num[k] + " ");
+                    sum += num[k];
+                }
+                maxSum = Math.max(maxSum,sum);
+                minSum = Math.min(minSum,sum);
+                System.out.print("sum : "+ sum);
+                ts++;
+
+                System.out.println();
             }
             System.out.println();
         }
+        System.out.println("total subarray : "+ ts);
+        System.out.println("maxsub :"+ maxSum);
+        System.out.println("minsum: "+ minSum);
     }
+
+    // public static void printPairs(int num[]) {
+    //     for (int i = 0; i < num.length; i++) {
+    //         int curr = num[i];
+    //         for (int j = i + 1; j < num.length; j++) {
+    //             System.out.print(("(" + curr + "," + num[j] + ")"));
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
     public static void main(String[] args) {
-        int num[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-        printPairs(num);
+        int num[] = {12,-3,64,87,-23,7,91};
+        subArray(num);
     }
 }
