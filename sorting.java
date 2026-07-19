0fcbc73fabf26a2ba97a7a49dca89274aa2e6835
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-07-19 17:55:25 +0530

    basic sorting

--- sorting.java
+++ sorting.java
@@ -20,15 +20,45 @@ public class sorting {
             if(!swapped){break;}
         }
     }
+   
+    public static void selectionSort(int arr[]){
+        int n = arr.length;
+        for(int i= 0;i<n;i++){
+            int minIdx = i;
+            for(int j =i+1;j<n;j++){
+                if(arr[j]<arr[minIdx]){
 
-    public static void printArray(int arr[]) {
+                    minIdx = j;
+                }
+            }
+            swapArr(arr, i, minIdx);
+        }
+    }
+
+    public static void insertionSort(int arr[]){
+        int n = arr.length;
+        for(int i= 1;i<n;i++){
+            int key = arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+    } 
+    
+
+     public static void printArray(int arr[]) {
         for (int num : arr){
             System.out.print(num + " ");
         }
     }
+    
     public static void main(String[] args) {
-        int arr[] = {3,5,1,7,2};
-        bubbleSort(arr);
+        int arr[] = {7,3,8,2,6,4,5};
+        // bubbleSort(arr);
+        insertionSort(arr);
         printArray(arr);
     }
 }
