354843ca0f436e17db812edf6fbf82ac91d0c79d
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-08-07 19:22:50 +0530

    LL zig zag

--- LL/LinkedList.java
+++ LL/LinkedList.java
@@ -321,7 +321,43 @@ public class LinkedList {
         return merge(newLeft, newRigth);
     }
 
-    
+    public void zigZag(){
+        // slow-fast mid
+        Node slow = head;
+        Node fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;;
+        }
+        Node mid = slow;
+
+        // reverse 
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+
+        while(left != null && right != null){
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+    }
 
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
@@ -332,7 +368,7 @@ public class LinkedList {
         ll.addFirst(8);
         ll.addFirst(11);
         ll.print();
-        ll.head = ll.mergeSort(ll.head);
+        ll.zigZag();
         ll.print();
 
     }
