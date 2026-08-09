7840a0d2c13d70357ab2d12761879a328ae496c2
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-08-10 02:06:57 +0530

    array and sorting basic done

--- array/sorting.java
+++ array/sorting.java
@@ -56,10 +56,31 @@ public class sorting {
         }
     }
     
+    public static void countingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int arr[] = {7,3,8,2,6,4,5};
+        int arr[] = {1,4,1,3,2,4,3,7};
         // bubbleSort(arr);
-        insertionSort(arr);
+        // insertionSort(arr);
+        countingSort(arr);
         printArray(arr);
     }
 }
