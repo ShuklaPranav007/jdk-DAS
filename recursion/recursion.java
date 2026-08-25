fb6221f6e023c4498b8542cd12f9eeaa4e54a6a1
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-08-26 00:53:53 +0530

    rec sum and fact

--- recursion/recursion.java
+++ recursion/recursion.java
@@ -8,7 +8,24 @@ public class recursion {
         printDec(n-1);
         System.out.print(n + " ");
     }
+
+
+    public static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        return n * fact(n-1);
+    }
+
+    public static int  sumRec(int n){
+        if(n==1){
+            return 1;
+        }
+        return n + sumRec(n-1);
+    }
     public static void main(String[] args) {
-        printDec(10);
+        // printDec(10);
+        // System.out.println(fact(4));
+        System.out.println("sum : "+ sumRec(10));
     }
 }
