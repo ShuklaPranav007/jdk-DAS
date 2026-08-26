4fb9b0e0d9298218e64c2ff4f417702e7504d3f3
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-08-26 17:24:19 +0530

    fibo

--- recursion/recursion.java
+++ recursion/recursion.java
@@ -9,7 +9,6 @@ public class recursion {
         System.out.print(n + " ");
     }
 
-
     public static int fact(int n){
         if(n==0){
             return 1;
@@ -23,9 +22,17 @@ public class recursion {
         }
         return n + sumRec(n-1);
     }
+
+    public static int fibo(int n){
+        if(n==0 || n ==1){
+            return n;
+        }
+        return fibo(n-1)+fibo(n-2);
+    }
     public static void main(String[] args) {
         // printDec(10);
         // System.out.println(fact(4));
-        System.out.println("sum : "+ sumRec(10));
+        // System.out.println("sum : "+ sumRec(10));
+        System.out.println(fibo(25));
     }
 }
