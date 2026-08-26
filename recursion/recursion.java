ba902acd248a536f00b5dd66c249a38fe5ce6c4f
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-08-27 00:25:09 +0530

    print binary non consecutive

--- recursion/recursion.java
+++ recursion/recursion.java
@@ -92,12 +92,23 @@ public class recursion {
         return friendsPair(n-1) + (n-1)*friendsPair(n-2);
     }
 
+    public static void printBinarySring(int n, int lastPlace, String str){
 
-    
+        if(n==0){
+            System.out.println(str);
+            return;
+        }
+        printBinarySring(n-1, 0, str+"0");
+        if(lastPlace== 0){
+            printBinarySring(n-1, 1, str+"1");
+        }
+    }
+
     public static void main(String[] args) {
         // String str = "anpanaksnddncbvbc";
         // removeDuplicateString(str, 0, new StringBuilder(""), new boolean[26]);
-        System.out.println(friendsPair(3));
+        // System.out.println(friendsPair(3));
+        printBinarySring(3, 0, "");
 
     }
 }
