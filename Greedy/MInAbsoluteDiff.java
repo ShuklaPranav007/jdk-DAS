7ced54e5f2e3c3743e160468f4c13ebf7c568922
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-08-29 19:30:46 +0530

    min abs

---
+++ Greedy/MInAbsoluteDiff.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class MInAbsoluteDiff {
+    public static void main(String[] args) {
+        int A[] = {4,1,8,7};
+        int B[] = {2,3,6,5};
+        int minDiff = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for(int i = 0; i<A.length;i++){
+            minDiff += Math.abs(A[i]- B[i]);
+        }
+        System.out.println("minDiff : " + minDiff);
+    }
+}
