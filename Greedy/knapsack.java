39586a9c8e37088f523f541f2149c714b2b850a3
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-08-29 16:58:05 +0530

    fractional knapsac

---
+++ Greedy/knapsack.java
@@ -0,0 +1,39 @@
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class knapsack {
+    public static void main(String[] args) {
+
+        int val[] = { 60, 100, 120 };
+        int weight[] = { 10, 20, 30 };
+        int W = 50;
+
+        double ratio[][] = new double[val.length][2];
+
+        for (int i = 0; i < val.length; i++) {
+            ratio[i][0] = i;
+            ratio[i][1] = val[i] / (double) weight[i];
+        }
+
+        // acending sorting
+        Arrays.sort(ratio,Comparator.comparingDouble(o->o[1]));
+
+
+        int capacity = W;
+        int value = 0;
+        for(int i = ratio.length-1;i>=0;i--){
+            int idx = (int)ratio[i][0];
+            if(capacity >= weight[idx]){
+                value += val[idx];
+                capacity -= weight[idx];
+            }else{
+                value += (ratio[i][1]*capacity);
+                capacity = 0;
+                break;
+            }
+        }
+        System.out.println("final value knapsack is: "+value);
+        
+    }
+}
