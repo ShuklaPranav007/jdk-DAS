71e561ba1a6b2aecfe931687b426d0dbafb8bd99
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-09-03 00:19:23 +0530

    tabulation for climbing stairs

--- DP/fibo/Climbing_Stairs.java
+++ DP/fibo/Climbing_Stairs.java
@@ -16,10 +16,31 @@ public class Climbing_Stairs {
         dp[n] = countWays(n-1, dp)+countWays(n-2, dp);
         return dp[n];
     }
+
+    // tabulation
+    // create table + initilize
+    // meaning of index
+    // fill(small to large)
+    public static int countWaysTab(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+
+        // tabulation loop
+        for(int i =1;i<= n;i++){
+            if(i==1){
+                dp[i]= dp[i-1]+0;
+            }else{
+                dp[i] = dp[i-1]+ dp[i-2];
+            }
+        }
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n = 5;
         int dp[] = new int[n+1];
         Arrays.fill(dp, -1);
         System.out.println(countWays(n, dp));
+        System.out.println(countWaysTab(n));        
+
     }
 }
