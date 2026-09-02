7e35255015eb32bc377cd994ea2117500be1a2a5
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-09-02 23:24:39 +0530

    fibo

--- Striver/Striver.java
+++ Striver/Striver.java
@@ -3,21 +3,29 @@ public class Striver {
     public static int largestElement(int[] nums) {
         int max = Integer.MIN_VALUE;
         int secondMax = Integer.MIN_VALUE;
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i]>max){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
                 secondMax = max;
                 max = nums[i];
-            }else if(nums[i]>secondMax && nums[i]!= max){
+            } else if (nums[i] > secondMax && nums[i] != max) {
                 secondMax = nums[i];
             }
         }
         return secondMax;
     }
-    public static void sorted(){}
 
+    public static boolean sorted(int nums[]) {
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i]<nums[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
-        int nums[] = { 7, 3, 6, 5,20,89, 1 };
-        System.out.println(largestElement(nums));
+        int nums[] = {2,7,4,7,3,2,2,5,7 };
+        // System.out.println(largestElement(nums));
+        System.out.println(sorted(nums));
     }
 }
