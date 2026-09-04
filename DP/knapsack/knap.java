323165dd3ea4e7226b4aa8e21d62d08f1362deee
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-09-04 13:19:31 +0530

    memoization code in 0-1 kknapsack

--- DP/knapsack/knap.java
+++ DP/knapsack/knap.java
@@ -1,26 +1,41 @@
 
 
 public class knap {
-    public static int knapsack(int val[], int wt[], int W, int n){
+    public static int knapsack(int val[], int wt[], int W, int n,int dp[][]){
         if(W == 0 || n == 0){
             return 0;
         }
+
+        if(dp[n][W] != -1){
+            return dp[n][W];
+        }
+
         if(wt[n-1]<=W){//valid
             //include 
-            int ans1 = val[n-1]+knapsack(val, wt, W-wt[n-1], n-1);
+            int ans1 = val[n-1]+knapsack(val, wt, W-wt[n-1], n-1, dp);
             
             // exlude 
-            int ans2 = knapsack(val, wt, W, n-1);
-            return Math.max(ans1, ans2);
+            int ans2 = knapsack(val, wt, W, n-1, dp);
+            dp[n][W] = Math.max(ans1, ans2);
+            return dp[n][W];
         }else{
             // not valid
-            return knapsack(val, wt, W, n-1);
+            dp[n][W] = knapsack(val, wt, W, n-1, dp);
+            return dp[n][W];
         }
     }
     public static void main(String[] args) {
-        int val[] = {15,14,10,45,30};
+        int val[] = {15,14,10,4,5};
         int wt[] = {2,5,1,3,4};
         int W = 7;
-        System.out.println(knapsack(val, wt, W, val.length));
+
+        int dp[][] = new int[val.length+1][W+1];
+        for(int i = 0; i<dp.length;i++){
+            for(int j = 0; j<dp[0].length;j++){
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(knapsack(val, wt, W, val.length,dp));
     }
 }
